aa8d4668e73f2601008009ec7c048579121fe874
TahReEm <tahreemhossain0@gmail.com>
2025-03-01 00:35:19 +0600

    main part completed

--- script/others.js
+++ script/others.js
@@ -12,3 +12,13 @@ function displayDate() {
   displayDate();
 
 
+  document.getElementById("bg-change").addEventListener("click", function(){
+    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
+});
+
+
+document.getElementById("blog").addEventListener("click", function(){
+    window.location.href = "blog.html";
+});
+
--- script/script.js
+++ script/script.js
@@ -1,10 +1,40 @@
-document.getElementById("bg-change").addEventListener("click", function(){
-    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-    document.body.style.backgroundColor = randomColor;
-});
+const buttons = document.querySelectorAll(".complete");
+let activitiesLog = document.getElementById("act-log");
+let clickedCount = 0;
+let taskName = "";
+let title = document.getElementsByTagName("h1");
+
+for(let j = 0; j < title.length; j++){
+    taskName = title[j].innerText;
+}
 
 
-document.getElementById("blog").addEventListener("click", function(){
-    window.location.href = "blog.html";
-});
 
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+        newLog = document.createElement("div");
+        let date = new Date().toLocaleString();
+        newLog.innerHTML = `
+        <p class="m-2 p-2">You have Complete ${taskName} at ${date}</p>
+        `
+        newLog.classList.add('bg')
+        newLog.classList.add('rounded-lg')
+        newLog.classList.add('mx-4')
+        newLog.classList.add('shadow-md')
+        activitiesLog.appendChild(newLog)
+
+        buttons[i].disabled = true;
+        buttons[i].innerText = "Completed";
+
+        alert("Board updated successfully")
+
+        clickedCount++;
+        if( clickedCount===buttons.length){
+            alert("Congrats, you've completed all the tasks!");
+        }
+    });
+}
+
+document.getElementById("clear").addEventListener("click", function(){
+    activitiesLog.innerHTML = "";
+})
